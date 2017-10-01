5b168a47c29c4c808ac193443292423e6ee38b47
Karim Aly <karimalyazzam@gmail.com>
2017-10-01 22:18:58 +0200

    Added a dummy hello ViewSet and a Router.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -55,3 +56,18 @@ class HellpApiView(APIView):
         """Deletes an object"""
 
         return Response({'method': 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_view_set = [
+            'User actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_view_set': a_view_set})
