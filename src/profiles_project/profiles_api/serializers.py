f15a9f9e2ac5306ee760a484d0bfb8babeeaaa00
Karim Aly <karimalyazzam@gmail.com>
2017-10-01 21:57:42 +0200

    Added POST, PUT, PATCH and DELETE functions to the dummy Hello APIView.

---
+++ src/profiles_project/profiles_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class HelloSerialzier(serializers.Serializer):
+    """Serializes a name field for testing our APIView"""
+
+    name = serializers.CharField(max_length=10)
+
+    
